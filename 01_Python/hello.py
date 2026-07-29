ded17b2635d380978b63b1e9c6bf780afa0e7ff6
Hamna-Nasir <hamnanasir.10r@gmail.com>
2026-07-29 16:41:11 +0500

    Week 1 Day 2: Variables and Data Types

---
+++ 01_Python/hello.py
@@ -0,0 +1,3 @@
+print("Hello, AI Engineer!")
+print("My name is Hamna.")
+print("Today is Day 1 of my AI journey.")
---
+++ 01_Python/variables.py
@@ -0,0 +1,22 @@
+# MINI CHALLENGE 1
+name = "Hamna"
+age = 21
+cgpa = 3.9
+is_student = True
+print("Hello My name is ", name , " .")
+print("I am ", age , "years old .")
+print("My CGPA is ", cgpa , " .")
+print("Student status : ", is_student)
+
+# MINI CHALLENGE 2
+favorite_food = "Pasta"
+favorite_color = "Blue"
+hobby = "Crochetting"
+city = "Karachi"
+country = "Pakistan"
+dream_job = "AI Engineer"
+graduation_year = 2027
+favorite_movie = "SpiderMan"
+favorite_language = "Python"
+favorite_season = "Spring"
+print("My favorite food is ", favorite_food , " ." , "My favorite color is ", favorite_color , " ." , "My hobby is ", hobby , " ." , "I live in ", city , " ," , country , " ." , "My dream job is to become an ", dream_job , " ." , "I will graduate in the year ", graduation_year , " ." , "My favorite movie is ", favorite_movie , " ." , "My favorite programming language is ", favorite_language , " ." , "My favorite season is ", favorite_season , " .")
