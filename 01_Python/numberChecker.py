939ad0f070d07d7f35712fd2ee8ed65c469a7dd4
Hamna-Nasir <hamnanasir.10r@gmail.com>
2026-07-30 16:52:15 +0500

    Week 1

--- 01_Python/numberChecker.py
+++ 01_Python/numberChecker.py
@@ -10,4 +10,9 @@ if num == 0:
     
     
     
-# Odd 
+# Odd & Even Checker
+num = int(input("Enter a number: "))
+if num % 2 == 0:
+    print("The number is even.")    
+else:
+    print("The number is odd.")
