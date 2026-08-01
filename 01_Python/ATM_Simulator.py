c5ad7899587908736be2f19ab090f1996fb15edb
Hamna-Nasir <hamnanasir.10r@gmail.com>
2026-08-01 15:44:53 +0500

    Week2

--- 01_Python/ATM_Simulator.py
+++ 01_Python/ATM_Simulator.py
@@ -4,5 +4,30 @@ print("=================")
 print("1. Check balance")
 print("2. Deposit")
 print("3. Withdraw")
-print("4. Exit")
 
+balance = 1000000
+
+
+options = int(input("Enter your choice: "))
+if options == 1:
+    print(f"Your balance is: {balance}")
+elif options == 2:
+    try:
+        deposit = int(input("Enter amount to deposit: "))
+        if deposit < 0:
+            raise ValueError("Deposit amount cannot be negative!")
+        balance += deposit
+        print(f"Your new balance is: {balance}")
+    except ValueError as e:
+        print(e)
+elif options == 3:
+    try:
+        withdraw = int(input("Enter amount to withdraw: "))
+        if withdraw < 0:
+            raise ValueError("Withdraw amount cannot be negative!")
+        if withdraw > balance:
+            raise ValueError("Insufficient funds!")
+        balance -= withdraw
+        print(f"Your new balance is: {balance}")
+    except ValueError as e:
+        print(e)
